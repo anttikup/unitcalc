4bfb1b9708f23ce9b906cd702216aa7a5cddf4b2
AK <anttikup@metropolia.fi>
2023-05-01 15:38:44 +0300

    loppu

--- src/main/scala/livechart/LiveChart.scala
+++ src/main/scala/livechart/LiveChart.scala
@@ -21,10 +21,41 @@ object Main {
   val model = new Model
   import model.*
 
+  val chartConfig =
+    import typings.chartJs.mod.*
+    new ChartConfiguration {
+      `type` = ChartType.bar
+      data = new ChartData {
+        datasets = js.Array(
+          new ChartDataSets {
+            label = "Price"
+            borderWidth = 1
+            backgroundColor = "green"
+          },
+          new ChartDataSets {
+            label = "Full price"
+            borderWidth = 1
+            backgroundColor = "blue"
+          }
+        )
+      }
+      options = new ChartOptions {
+        scales = new ChartScales {
+          yAxes = js.Array(new CommonAxe {
+            ticks = new TickOptions {
+              beginAtZero = true
+            }
+          })
+        }
+      }
+    }
+  end chartConfig
+
   def appElement(): Element =
     div(
       h1("Live Chart"),
       renderDataTable(),
+      renderDataChart(),
       renderDataList(),
     )
   end appElement
@@ -113,12 +144,13 @@ object Main {
       valueUpdater: Observer[Double]): Input =
     val strValue = Var[String]("")
     input(
+      width := "3em",
       typ := "text",
       value <-- strValue.signal,
       onInput.mapToValue --> strValue,
       valueSignal --> strValue.updater[Double] { (prevStr, newValue) =>
         if prevStr.toDoubleOption.contains(newValue) then prevStr
-        else newValue.toString
+        else "%.2f".format(newValue)
       },
       strValue.signal --> { valueStr =>
         valueStr.toDoubleOption.foreach(valueUpdater.onNext)
@@ -130,6 +162,7 @@ object Main {
   def inputForInt(valueSignal: Signal[Int],
       valueUpdater: Observer[Int]): Input = {
     input(
+      width := "3em",
       typ := "text",
       controlled(
         value <-- valueSignal.map(_.toString),
@@ -140,4 +173,42 @@ object Main {
     )
   }
 
+  def renderDataChart(): Element =
+    import scala.scalajs.js.JSConverters.*
+    import typings.chartJs.mod.*
+
+    var optChart: Option[Chart] = None
+
+    canvasTag(
+      // Regular properties of the canvas
+      width := "100%",
+      height := "200px",
+
+      // onMountUnmount callback to bridge the Laminar world and the Chart.js world
+      onMountUnmountCallback(
+        // on mount, create the `Chart` instance and store it in optChart
+        mount = { nodeCtx =>
+          val domCanvas: dom.HTMLCanvasElement = nodeCtx.thisNode.ref
+          val chart = Chart.apply.newInstance2(domCanvas, chartConfig)
+          optChart = Some(chart)
+        },
+        // on unmount, destroy the `Chart` instance
+        unmount = { thisNode =>
+          for (chart <- optChart)
+            chart.destroy()
+          optChart = None
+        }
+      ),
+
+      // Bridge the FRP world of dataSignal to the imperative world of the `chart.data`
+      dataSignal --> { data =>
+        for (chart <- optChart) {
+          chart.data.labels = data.map(_.label).toJSArray
+          chart.data.datasets.get(0).data = data.map(_.price).toJSArray
+          chart.data.datasets.get(1).data = data.map(_.fullPrice).toJSArray
+          chart.update()
+        }
+      },
+    )
+  end renderDataChart
 }
